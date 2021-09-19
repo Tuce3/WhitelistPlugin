2a7ae7e4ba2e5e0b3538b47c36fc3382c719789c
BuildTools <tuxe5335@gmail.com>
2021-09-19 17:33:39 +0200

    Initial commit

---
+++ src/me/tuce/Whitelist/Main.java
@@ -0,0 +1,41 @@
+package me.tuce.Whitelist;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class Main extends JavaPlugin implements Listener {
+
+    private File customConfigFile;
+    private FileConfiguration customConfig;
+
+    @Override
+    public void onEnable() {
+        createCustomConfig();
+
+        new PlayerJoin(this);
+    }
+
+    public FileConfiguration getCustomConfig() {return this.customConfig;}
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "config.yml");
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+
+        customConfig = new YamlConfiguration();
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+}
